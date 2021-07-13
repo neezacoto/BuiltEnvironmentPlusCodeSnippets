479b977847babe99913f99e6c04776c21274cebb
Christian Rudder <74576449+neezacoto@users.noreply.github.com>
2021-07-13 16:59:25 -0400

    Update course-selection.js

--- Scripts/course-selection.js
+++ Scripts/course-selection.js
@@ -1,11 +1,27 @@
 <script>
   
-  
 	jQuery(document).on('ready ajaxComplete', function () {
     
     //course field
     let course_field = document.getElementById("course-field");
     course_field.placeholder = "LEED v4 Project Management,LEED for Homes Workshop: Part2,...";
+    
+    jQuery(".wpcf7").wrap('<div id="form-container"/>');
+    
+    //add tag container
+    jQuery(".wpcf7").before('<div class="padder"><div id="course-tag-container"/></div>');
+    
+    //add mockup thing
+    jQuery("#course-tag-container").append('<div class="tag"><div class="course-wrap"><p class="course">bruh</p><div class="input-wrapper"><input type="image" name="removeButton" src="http://builtenvironmentplus.org/wp-content/uploads/2021/07/course_remove.svg" alt="remove button" class="remove-button"> </div></div></div>');
+    
+    jQuery(".remove-button").click((event)=>{
+      event.target.closest(".tag").remove();
+    });
+    
+    
+    
+    //add new fake submit button
+    jQuery("#form-container").append('<div class="padder"><button id="fake-submit" type="button">Send</button></div>');
     
 		//add button functionality
     jQuery(".et_pb_post").wrap('<div class="course-wrapper" />');      
@@ -13,12 +29,19 @@
     jQuery(".add-button").wrap('<div/>');
     
     //adding button functionality   
-    jQuery(".add-button").click(function(){    
-      let course_name = jQuery(this).parent().parent().find("h2.entry-title").text();
-       course_field.value += (course_field.value =='')? course_name : ", " + course_name;     
+    jQuery(".add-button").click(()=>{    
+     	let course_name = jQuery(this).closest(".course-wrapper").find(".entry-title").text();
+      console.log(course_name);
+      //add course
+      jQuery("#course-tag-container").append('<div class="tag"><div class="course-wrap"><p class="course">'+course_name+'</p><div class="input-wrapper"><input type="image" name="removeButton" src="http://builtenvironmentplus.org/wp-content/uploads/2021/07/course_remove.svg" alt="remove button" class="remove-button"> </div></div></div>');
+      
+      jQuery(".remove-button").click((event)=>{
+      event.target.closest(".tag").remove();
+    });
+       
     })
     
-    //remove button functionality
+    
 		
 	}); 
 </script>
