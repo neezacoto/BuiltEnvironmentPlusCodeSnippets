61e8d264617fddd0eae43a5ce4674ba569a1d81e
Christian Rudder <74576449+neezacoto@users.noreply.github.com>
2021-07-27 12:46:42 -0400

    Update course-selection.js

--- Scripts/course-selection.js
+++ Scripts/course-selection.js
@@ -1,65 +1,384 @@
-<style>
-
- .show-testimonial {
-visibility: visible !important;
-opacity: 1 !important;
-top: 0 !important;
-}
- 
-.testimonial-active {
-transform: translateX(-10%);
-}
- 
-[id*="testimonial-person"]{
--webkit-transition: all 0.5s ease;
--moz-transition: all 0.5s ease;
--o-transition: all 0.5s ease;
--ms-transition: all 0.5s ease;
-transition: all 0.5s ease;
- 
-cursor: context-menu;
-}
- 
-[id*="testimonial-copy"] {
--webkit-transition: all 0.3s ease;
--moz-transition: all 0.3s ease;
--o-transition: all 0.3s ease;
--ms-transition: all 0.3s ease;
-transition: all 0.3s ease;
- 
-position: absolute!important;
-top: -100px;
-bottom: auto;
-left: 0;
-right: auto;
- 
-visibility: hidden;
-opacity: 0;
-}
-  
-</style>
 
 <script>
+let hidden = false; 
+let counter = 0;
+  
+jQuery(document).ready(function () {
 
- jQuery(function($){
-$(document).ready(function(){
- 
-$('#testimonial-person-1').addClass('testimonial-active');
- 
-$('[id*="testimonial-person"]').hover(function() {
- 
-var $selector = $(this).attr('id').replace('person', 'copy');
-var $testimonial = $('#' + $selector);
- 
-$('[id*="testimonial-copy"]').removeClass('show-testimonial');
-$testimonial.addClass('show-testimonial');
- 
-$('[id*="testimonial-person"]').removeClass('testimonial-active');
-$(this).addClass('testimonial-active');
- 
-});
- 
-});
+	//course field
+	let course_field = document.getElementById("course-field");
+	course_field.placeholder = "LEED v4 Project Management,LEED for Homes Workshop: Part2,...";
+
+	jQuery(".wpcf7").wrap('<div id="form-container"/>');
+
+	//add tag container
+	jQuery(".wpcf7").before('<p class="tag-label">Selected Course<span style="font-size: 18px">(s)</span><span style="color:red;">*</span></p><div id="course-tag-container"><p id="preview-text" style="color: #ababab; font-size: 15px;">Press The Plus Icon to Add a Course</p></div>');
+
+	jQuery("#submit-button").before('<button id="fake-submit" type="button">Send</button>');
+
+	jQuery("#fake-submit").click((event) => {
+
+        let course_inv = [];
+
+        let tags = jQuery(".bold-me").toArray();
+        let radios = jQuery(".radio__input:checked").toArray();
+        let totals = jQuery(".total").toArray();
+
+        for(let i = 0; i < tags.length; i++)
+        {
+            entry = "";
+            entry += jQuery(tags[i]).text().trim();
+            entry += "|" + jQuery(radios[i]).closest("label").text().trim();
+            entry += "|" + jQuery(totals[i]).val();
+            course_inv.push(entry);
+        }
+
+
+        jQuery("#course-field").val("");
+		    jQuery("#course-field").val(course_inv.join(","));
+		    jQuery("#submit-button").click();
+      })
+
 });
 
+jQuery(document).on('ready ajaxComplete', function () {
+
+	//add button functionality
+	jQuery(".et_pb_post").wrap('<div class="course-wrapper" />');
+	jQuery(".et_pb_post").after('<input type="image" name="addButton" src="http://builtenvironmentplus.org/wp-content/uploads/2021/07/course_add.svg" width="100px" height="auto" alt="add button" class="add-button">');
+	jQuery(".add-button").wrap('<div/>');
+
+	//adding button functionality   
+	jQuery(".add-button").click((event) => {
+    
+    
+    
+		let course_name = jQuery(event.target).closest(".course-wrapper").find(".entry-title").text();
+
+		if (!jQuery(".tag:contains('" + course_name + "')").length) {
+      
+      if(!hidden){
+        jQuery("#preview-text").addClass('hide');
+        hidden= true;
+        window.onbeforeunload = function() {
+    			return true;
+				}
+      }
+      counter++;
+      let tagsNum = counter;
+      let radioNum = tagsNum * 3;
+      
+      let btn =  "btn-"+tagsNum;
+      let radio1 = "radio-"+(radioNum-3);
+      let radio2 = "radio-"+(radioNum-2);
+      let radio3 = "radio-"+(radioNum-1);
+      
+
+			jQuery("#course-tag-container").append('<nav class="tag"> <label for="'+btn+'" class="button bold-me highlight" onclick="rotate(event)">'+course_name+'<span class="fas fa-caret-down arrow" ></span> </label><input type="checkbox" id="'+btn+'" checked> <ul class="menu"> <li> <label for="" class="title course">Express Program Preference:</label> <label for="'+radio1+'" class="radio"> <input type="radio" name="'+course_name+'" id="'+radio1+'" class="radio__input"> <div class="radio__radio"></div> A Must </label> <label for="'+radio2+'" class="radio"> <input type="radio" name="'+course_name+'" id="'+radio2+'" class="radio__input"> <div class="radio__radio"></div> Would Like </label> <label for="'+radio3+'" class="radio"> <input type="radio" name="'+course_name+'" id="'+radio3+'" class="radio__input" checked> <div class="radio__radio"></div> No Preference </label> </li> <li> <div class="divider"></div> </li> <li> <label for="" class="course-title">Expected Party Size:</label> <i class="fas fa-user"></i><input type="number" name="" value="1" min="1" max="9999999" class="people total" onkeydown="maxNumber(event)"> <button onclick="minus(event)" class="a">-</button> <button onclick="add(event)" class="b">+</button> </li> <li> <div class="divider"></div> </li> <li> <button onclick="tagDelete(event)" class="remove-course">Remove Course</button> </li> </ul></nav>');
+      
+
+		} else {
+			jQuery(".tag:contains('" + course_name + "'),.tag:contains('" + course_name + "') .menu").addClass('animate')
+      
+			jQuery(".tag:contains('" + course_name + "'),.tag:contains('" + course_name + "') .menu").on("animationend", () => {
+				jQuery(".tag:contains('" + course_name + "'),.tag:contains('" + course_name + "') .menu").removeClass('animate');
+			});
+		}
+	})
+  
+  
+
+});
+
+function tagDelete(event) {
+
+	event.target.closest(".tag").remove();
+
+  
+  if(hidden && !jQuery(".tag").length){
+        jQuery("#preview-text").removeClass('hide');
+    		hidden= false;
+    		window.onbeforeunload = null;
+      }
+
+}
+  
+			function rotate(event){
+        
+        jQuery(event.target).closest(".bold-me").toggleClass("highlight");
+        jQuery(event.target).closest(".bold-me").find(".arrow").toggleClass("rotate");
+        
+        
+      }
+
+      function add(event){
+        
+            var sign = jQuery(event.target).closest("li").find(".total").val();
+        		if(sign < 999999) {
+            var sum = sign - (-1);
+            jQuery(event.target).closest("li").find(".total").val(sum);
+      }   
+      }
+
+      function minus(event){
+        
+            var sign = jQuery(event.target).closest("li").find(".total").val();
+            if(sign > 1) {
+            var sum = sign - 1;
+            jQuery(event.target).closest("li").find(".total").val(sum);
+        
+      }
+      }
+  
+  		function maxNumber (event){
+
+        if(parseInt(jQuery(event.target).val()) > 999999){
+          jQuery(event.target).val(999999);
+        }
+      }
+        
+      	
+        
+  
 </script>
+
+<style>
+
+label.button {
+  position: relative; /* <-- causes the label to act as a container */
+  padding-right: 20px;
+}
+
+.fa-caret-down {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+}
+
+nav.tag {
+  background: #DBDBDB;
+  width: 100%;
+  line-height: 40px;
+  padding: 5px 0px 5px;
+  border-radius: 5px 5px 5px 5px;
+  -webkit-touch-callout: none;
+  /* iOS Safari */
+  -webkit-user-select: none;
+  /* Safari */
+  -khtml-user-select: none;
+  /* Konqueror HTML */
+  -moz-user-select: none;
+  /* Old versions of Firefox */
+  -ms-user-select: none;
+  /* Internet Explorer/Edge */
+  user-select: none;
+  margin-bottom: 5px;
+  margin-top: 5px;
+}
+
+label.button {
+  line-height: 130%
+}
+
+.button {
+  padding-left: 20px;
+}
+
+.tag input[type='checkbox'] {
+  display: none;
+}
+
+nav label {
+  font-size: 20px;
+  display: block;
+  cursor: pointer;
+}
+
+.button span {
+  float: right;
+  line-height: 130%;
+  margin-right: 20px;
+  margin-top: 0px;
+}
+
+nav ul label:nth-child(1) {
+  font-weight: bold;
+  font-size: 20px;
+}
+
+nav.tag ul {
+  background: #DBDBDB;
+  list-style: none;
+  width: 100%;
+  border-radius: 0px 0px 5px 5px;
+  padding-bottom: 5px;
+  display: none;
+}
+
+.bold-me {
+  transition: 30ms;
+}
+
+.highlight {
+  font-weight: bold;
+  color: #005DA4;
+  font-size: 21px;
+}
+
+[id^=btn]:checked+ul {
+  display: block;
+}
+
+nav.tag ul li {
+  line-height: 40px;
+  padding 8px 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+nav.tag ul li:nth-child(5) {
+  margin-top: 0px;
+}
+
+.divider {
+  width: 80%;
+  height: 2px;
+  background-color: #BBBBBB;
+}
+
+label.title.course {
+    cursor:context-menu !important;
+}
+
+label.course-title {
+  	font-size: 20px;
+    cursor:context-menu !important
+}
+  
+.people{
+  border: solid 1px #BBBBBB;
+  height: 32px;
+  width: 119px;
+  padding: 0px 0px 0px 20px;
+  font-size: 17px;
+  outline: none;
+  border: none;
+  transition: 100ms;
+  border-radius: 5px;
+  transform: translateY(-1.5px);
+}
+
+.people:focus{
+  box-shadow: 0 0 3pt 1pt cornflowerblue;
+
+}
+
+.radio {
+  display: inline-flex;
+  align-items: center;
+  cursor: pointer;
+  margin-right: 10px;
+
+}
+
+.radio__input {
+  display: none;
+
+}
+.radio__radio{
+  width: 25px;
+  height: 25px;
+  border: 3px solid rgb(103, 103, 103);
+  border-radius: 50%;
+  margin-right: 10px;
+  box-sizing: border-box;
+  padding: 2px;
+  transition: 150ms;
+  background: #f4f4f4
+
+}
+
+.a,.b{
+  border: none;
+  border-radius: 5px;
+  font-size: 23px;
+  font-weight: lighter;
+  width: 30px;
+  outline:none;
+  cursor: pointer;
+  transition: 120ms;
+}
+.a{
+  padding-bottom: 2px;
+}
+
+.a:active,.b:active{
+  background-color: #3382BE;
+}
+input[type=number]::-webkit-inner-spin-button,
+input[type=number]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+}
+
+
+.radio__radio:hover{
+  box-shadow: 0 0 3pt 1pt cornflowerblue;
+}
+.radio__radio::after {
+  content: "";
+  width: 100%;
+  height: 100%;
+  display: block;
+  background:#3382BE;
+  border-radius: 50%;
+
+  transform: scale(0);
+  transition: transform 150ms;
+}
+
+.radio__input:checked + .radio__radio::after{
+
+  transform: scale(1);
+
+}
+
+.remove-course{
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  color: #535353;
+  font-size: 17px;
+  font-weight: bold;
+  border-radius: 5px;
+  transition: 150ms;
+  padding: 5px;
+  padding-left: 10px;
+  padding-right: 10px;
+  text-decoration: underline;
+  outline: none;
+}
+
+.remove-course:hover{
+  
+  color: #3382BE;
+  outline: none;
+}
+#btn{
+  display: none;
+}
+.arrow{
+  transition: 250ms ease-out;
+}
+.rotate{
+  
+}
+.button span.rotate{
+  transform: rotate(-180deg);
+}
+.fa-user{
+  margin-right: 10px;
+  font-size: 24px;
+  transform: translateY(3px);
+}
+
+</style>
